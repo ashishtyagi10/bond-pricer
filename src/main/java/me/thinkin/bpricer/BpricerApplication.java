e7f9bdc867c4cc332d1276563a01baa41fcce42c
ashish <ashish@my-computer.lan1>
2018-09-12 20:26:58 -0400

    First commit towards big project

---
+++ src/main/java/me/thinkin/bpricer/BpricerApplication.java
@@ -0,0 +1,12 @@
+package me.thinkin.bpricer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpricerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BpricerApplication.class, args);
+    }
+}
---
+++ src/test/java/me/thinkin/bpricer/BpricerApplicationTests.java
@@ -0,0 +1,16 @@
+package me.thinkin.bpricer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BpricerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
